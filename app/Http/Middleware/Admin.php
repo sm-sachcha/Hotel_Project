5254f9a8ba31b95814165974f19eaced71352941
Shariar Mahmud Sachcha <114009659+sm-sachcha@users.noreply.github.com>
2026-02-22 16:16:32 +0600

    Middleware added

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Admin
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(Auth()->user()->usertype == 'admin') 
+            {
+                return $next($request);
+            }
+        abort(403, 'Unauthorized');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,12 @@ route::get("/", [AdminController::class,"home"]);
 
 route::get('/home', [AdminController::class,'index'])->name('home');
 
-route::get('/create_room', [AdminController::class,'create_room']);
+route::get('/create_room', [AdminController::class,'create_room'])->middleware((['auth','admin']));
+;
 
 route::post('/add_room', [AdminController::class,'add_room']);
 
-route::get('/view_room', [AdminController::class,'view_room']);
+route::get('/view_room', [AdminController::class,'view_room'])->middleware((['auth','admin']));
 
 route::get('/delete_room/{id}', [AdminController::class,'delete_room']);
 
@@ -27,7 +28,7 @@ route::get('/room_details/{id}', [HomeController::class,'room_details']);
 
 route::post('/add_booking/{id}', [HomeController::class,'add_booking']);
 
-route::get('/bookings', [AdminController::class,'bookings']);
+route::get('/bookings', [AdminController::class,'bookings'])->middleware((['auth','admin']));
 
 route::get('/delete_booking/{id}', [AdminController::class,'delete_booking']);
 
@@ -43,7 +44,7 @@ Route::get('/delete_gallery/{id}', [AdminController::class, 'delete_gallery']);
 
 Route::post('/contact', [HomeController::class, 'contact']);
 
-Route::get('/all_messages', [AdminController::class, 'all_messages']);
+Route::get('/all_messages', [AdminController::class, 'all_messages'])->middleware((['auth','admin']));
 
 Route::get('send_mail/{id}', [AdminController::class,'send_mail']);
 
